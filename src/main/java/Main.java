583383e56a6deb4721273fc84bb935dddd333b34
Gokul Jayavel <jayavel.g@northeastern.edu>
2024-11-23 19:09:53 -0500

    added api version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -32,10 +32,15 @@ public class Main {
              int correlationId = ((buffer[8] & 0xFF) << 24) | ((buffer[9] & 0xFF) << 16)
                      | ((buffer[10] & 0xFF) << 8) | (buffer[11] & 0xFF);
 
+             int requestApiVersion = ((buffer[6] & 0xFF) << 8) | (buffer[7] & 0xFF);
+
+
              System.err.println("Parsed correlation_id: " + correlationId);
 
+             System.err.println("Parsed request_api_version: " + requestApiVersion);
 
-             byte[] response = new byte[8];
+
+             byte[] response = new byte[10];
 
              // message_size (4 bytes)
              response[0] = 0x00;
@@ -48,6 +53,14 @@ public class Main {
              response[5] = (byte) ((correlationId >> 16) & 0xFF);
              response[6] = (byte) ((correlationId >> 8) & 0xFF);
              response[7] = (byte) (correlationId & 0xFF);
+
+             if (requestApiVersion > 4) {
+                 response[8] = 0x00;
+                 response[9] = 0x23; // UNSUPPORTED_VERSION (35)
+             } else {
+                 response[8] = 0x00;
+                 response[9] = 0x00; // No error
+             }
 
              OutputStream out = clientSocket.getOutputStream();
              out.write(response);
